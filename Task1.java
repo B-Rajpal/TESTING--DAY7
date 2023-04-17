82194a73d042dfdab214572ca4a57ed135958fe7
Rajpal B <118428308+B-Rajpal@users.noreply.github.com>
2023-04-17 14:10:57 +0530

    COMMIT

---
+++ Task1.java
@@ -0,0 +1,26 @@
+package Practice;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Task1{
+	
+	@Test
+	public void Testing() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver;
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		driver = new ChromeDriver(co);
+
+		driver.get("https://demo.wpeverest.com/user-registration/guest-registration-form/");
+		String url =driver.getTitle();
+		Assert.assertEquals(url, "Guest Registration Form – User Registration");
+	}
+}
